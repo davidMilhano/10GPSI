4d57afca2e6dac8f3946c5e700d8a0c87da02369
Alexandre <josealex1969@gmail.com>
2023-05-21 14:45:55 +0100

    Add project files.

---
+++ 10GPSI/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace _10GPSI
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bloco = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bloco)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bloco
+            // 
+            this.bloco.BackColor = System.Drawing.Color.Red;
+            this.bloco.Location = new System.Drawing.Point(362, 177);
+            this.bloco.Name = "bloco";
+            this.bloco.Size = new System.Drawing.Size(50, 50);
+            this.bloco.TabIndex = 0;
+            this.bloco.TabStop = false;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.bloco);
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Movimento";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmPrincipal_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.bloco)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bloco;
+    }
+}
+
---
+++ 10GPSI/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10GPSI
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    bloco.Top -= 10;
+                    break;
+                case Keys.Down:
+                    bloco.Top += 10;
+                    break;
+            }
+        }
+    }
+}
